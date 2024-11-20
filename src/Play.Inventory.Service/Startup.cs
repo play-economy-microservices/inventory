021a05537f2eff2ad3caa8257dd8464c16414bf6
Gio <t-giovannyh@Gios-MacBook-Pro.local>
2024-11-19 22:40:37 -0600

    Upgraded to MassTransit v8

--- src/Play.Inventory.Service/Startup.cs
+++ src/Play.Inventory.Service/Startup.cs
@@ -1,6 +1,6 @@
 using System;
 using System.Net.Http;
-using GreenPipes;
+using MassTransit;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
